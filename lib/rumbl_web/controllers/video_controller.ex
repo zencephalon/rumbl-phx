3036cae2d18522d0321cbc59e0ae69b170f8d011
Matthew Bunday <mkbunday@gmail.com>
2019-07-07 14:14:11 -0400

    User video management

--- lib/rumbl_web/controllers/video_controller.ex
+++ lib/rumbl_web/controllers/video_controller.ex
@@ -4,18 +4,23 @@ defmodule RumblWeb.VideoController do
   alias Rumbl.Multimedia
   alias Rumbl.Multimedia.Video
 
+  def action(conn, _) do
+    args = [conn, conn.params, conn.assigns.current_user]
+    apply(__MODULE__, action_name(conn), args)
+  end
+
   def index(conn, _params) do
-    videos = Multimedia.list_videos()
+    videos = Multimedia.list_user_videos()
     render(conn, "index.html", videos: videos)
   end
 
-  def new(conn, _params) do
+  def new(conn, _params, _current_user) do
     changeset = Multimedia.change_video(%Video{})
     render(conn, "new.html", changeset: changeset)
   end
 
-  def create(conn, %{"video" => video_params}) do
-    case Multimedia.create_video(video_params) do
+  def create(conn, %{"video" => video_params}, current_user) do
+    case Multimedia.create_video(current_user, video_params) do
       {:ok, video} ->
         conn
         |> put_flash(:info, "Video created successfully.")
@@ -27,18 +32,18 @@ defmodule RumblWeb.VideoController do
   end
 
   def show(conn, %{"id" => id}) do
-    video = Multimedia.get_video!(id)
+    video = Multimedia.get_user_video!(id)
     render(conn, "show.html", video: video)
   end
 
   def edit(conn, %{"id" => id}) do
-    video = Multimedia.get_video!(id)
+    video = Multimedia.get_user_video!(id)
     changeset = Multimedia.change_video(video)
     render(conn, "edit.html", video: video, changeset: changeset)
   end
 
   def update(conn, %{"id" => id, "video" => video_params}) do
-    video = Multimedia.get_video!(id)
+    video = Multimedia.get_user_video!(id)
 
     case Multimedia.update_video(video, video_params) do
       {:ok, video} ->
@@ -52,7 +57,7 @@ defmodule RumblWeb.VideoController do
   end
 
   def delete(conn, %{"id" => id}) do
-    video = Multimedia.get_video!(id)
+    video = Multimedia.get_user_video!(id)
     {:ok, _video} = Multimedia.delete_video(video)
 
     conn
