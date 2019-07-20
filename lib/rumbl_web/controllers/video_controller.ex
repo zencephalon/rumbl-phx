1e2cfc26cccc6c301878914f1f0377d4e80a6960
Matthew Bunday <mkbunday@gmail.com>
2019-07-20 09:25:23 -0400

    Test login gate functionality

--- lib/rumbl_web/controllers/video_controller.ex
+++ lib/rumbl_web/controllers/video_controller.ex
@@ -5,6 +5,7 @@ defmodule RumblWeb.VideoController do
   alias Rumbl.Multimedia.Video
 
   plug :load_categories when action in [:new, :create, :edit, :update]
+  plug :authenticate_user when action in [:index, :show]
 
   defp load_categories(conn, _) do
     assign(conn, :categories, Multimedia.list_alphabetical_categories())
